e724e72d7945c73de22cdfc4cbae56c6716d9b76
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-04 15:27:59 -0600

    removed unnecessary modules

--- client/src/components/userProfiles/UserProfileList.js
+++ client/src/components/userProfiles/UserProfileList.js
@@ -1,9 +1,13 @@
 import { useState } from "react"
+import { getUserProfiles } from "../../managers/profileManager";
 
 export const UserProfileList = () => {
     const [users, setUsers] = useState([]);
 
-    
+    const getAndSetAllUsers = () => {
+        getUserProfiles().then(setUsers);
+    }
+
 
     return (
     <div className="container">

faa8947538b8cc2c3de7c7c51514f666838fe6c3
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-08 16:28:04 -0600

    added home view

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,14 +5,13 @@ export const Home = () => {
             <h1>Home</h1>
         </div>
         <div className="body">
-            <div>
+            <div style={{marginBottom:30}}>
                 <h4>About</h4>
                 <p>House Rules is a Full-Stack .NET application built with a React.js client and C# with PostgreSQL server on the server-side.</p>
-                <p>The application is intended to be used by househole administrators, or anyone in the household who needs to view and/or complete chores.</p>
                 <p><span style={{fontWeight:"bold"}}>Administrator</span> users are able to view all data about chore details, chore assignments, chore completions, user profile details, and user chore assignments.</p>
                 <p><span style={{fontWeight:"bold"}}>All logged-in users</span> are able to view a list of chores to complete and when they were last completed, as well as a list of their individual assigned chores which are past due for completion. </p>
             </div>
-            <div>
+            <div style={{marginBottom:30}}>
                 <h4>Key Learning Objectives:</h4>
                 <ul>
                     <li>Authentication sing Identity Framework and EF Core</li>
@@ -23,24 +22,26 @@ export const Home = () => {
                     <li>Server-side validation</li>
                 </ul>
             </div>
-            <div>
+            <div style={{marginBottom:30}}>
                 <h4>Skills Used</h4>
                 <table>
-                    <tr>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/121405754-b4f48f80-c95d-11eb-8893-fc325bde617f.png" alt=".NET Core" title=".NET Core"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/117208740-bfb78400-adf5-11eb-97bb-09072b6bedfc.png" alt="PostgreSQL" title="PostgreSQL"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/></td>
-                    </tr>
-                    <tr>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183898054-b3d693d4-dafb-4808-a509-bab54cf5de34.png" alt="Bootstrap" title="Bootstrap"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192108891-d86b6220-e232-423a-bf5f-90903e6887c3.png" alt="Visual Studio Code" title="Visual Studio Code"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png" alt="GitHub" title="GitHub"/></td>
-                        <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/186711335-a3729606-5a78-4496-9a36-06efcc74f800.png" alt="Swagger" title="Swagger"/></td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/121405754-b4f48f80-c95d-11eb-8893-fc325bde617f.png" alt=".NET Core" title=".NET Core"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/117208740-bfb78400-adf5-11eb-97bb-09072b6bedfc.png" alt="PostgreSQL" title="PostgreSQL"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/></td>
+                        </tr>
+                        <tr>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/183898054-b3d693d4-dafb-4808-a509-bab54cf5de34.png" alt="Bootstrap" title="Bootstrap"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192108891-d86b6220-e232-423a-bf5f-90903e6887c3.png" alt="Visual Studio Code" title="Visual Studio Code"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png" alt="GitHub" title="GitHub"/></td>
+                            <td style={{padding:10}}><img width="60" src="https://user-images.githubusercontent.com/25181517/186711335-a3729606-5a78-4496-9a36-06efcc74f800.png" alt="Swagger" title="Swagger"/></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
